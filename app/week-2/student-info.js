0d90dd7048e216bb2464450950828607ff3a355a
hosumchan <152638966+hosumchan@users.noreply.github.com>
2024-05-21 15:16:34 -0600

    add student-info.js and update page.js

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <p>Ho Sum, CHAN</p>
+        <Link href="https://github.com/hosumchan/cprg306-assignments">https://github.com</Link>
+      </main>
+    );
+}
