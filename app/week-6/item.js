58a4d33acc89292e55a85b0cdd88418173af7713
hosumchan <152638966+hosumchan@users.noreply.github.com>
2024-06-26 22:23:04 -0600

    week-6 assignment re-do
    week-6 assignment re-do

--- app/week-6/item.js
+++
@@ -1,9 +0,0 @@
-
-export default function Item({name, quantity, category}) {
-    return (
-      <li className="bg-gray-800 text-white p-4 mb-4">
-          <div className="text-2xl font-bond">{name}</div>
-          <div className="text-xl"> Buy {quantity} in {category}</div>          
-      </li>
-    );
-}
